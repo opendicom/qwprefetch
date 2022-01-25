b081edf77910b8034e2efb29304c4d4fa3f851bf
jacquesfauquex <jacquesfauquex@gmail.com>
2022-01-25 13:31:06 -0300

    working

--- README.md
+++ README.md
@@ -5,16 +5,25 @@ dicom qido wado url prefetch
 
 ## Funciones
 
-Proxy between un pacs dicomweb y una estación de trabajo especializada dicom, que permite prefetch.
+Proxy entre un pacs dicomweb y una estación de trabajo especializada dicom, que permite prefetch.
 
 La aplicación está subdividida en dos procesos:
+
+### primer proceso
 
 - qido / wado que alimenta un directorio spool
 - storescu que encuentra los nuevos objetos dentro del spool y los manda a la estación de trabajo.
 
-La selección de los objetos está definida por una lista de una o más urls qido de nivel instancia.
+La selección de los objetos está definida por:
+- un url
+- una lista de instituciones
+- una fecha
 
-Se repiten periodicamente las invocaciones de estas urls, se comparan las listas obtenidas con los objetos ya presentes en el spool y se descargan por wado los objetos no encontrados en el spool.
+- Se repiten periodicamente las invocaciones de la composición de estos filtros, 
+- se comparan las listas obtenidas con los objetos ya presentes en el directorio spool 
+- y se descargan por wado los objetos no encontrados en el spool
+
+### segundo proceso
 
 storescu igualmente análisa el spool para encontrar los objetos todavía no recibidos por la estación de trabajo y enviarlos.
 
@@ -22,36 +31,37 @@ storescu igualmente análisa el spool para encontrar los objetos todavía no rec
 
 ## Estructura del spool
 
-- aetSource
+- transfer syntax
 
-  - día (aaaammdd)
+   - institución
 
-    - euid (Study Instance UID)
+      - día (aaaammdd)
 
-      - iuid (SOP InstanceUID inicialmente sin extensión, y luego con extensión ".done" o ".bad" depediendo del éxito del envio a la estación de trabajo) 
+        - euid (Study Instance UID)
+
+          - iuid (SOP InstanceUID inicialmente con extensión ".dcm", y luego con extensión ".done" o ".bad" depediendo del éxito del envio a la estación de trabajo) 
 
         
 
 ## Comando qwprefetch
 
 ```
-qwprefetch qidosJsonPath spoolDirPath [aaaammdd]
+qwprefetch qidourl institucion.json spoolDir [date]
 ```
 
-El tercer argumento es opcional. Si no está presente, por defecto se selecciona el día de hoy.
+El cuarto argumento es opcional. Si no está presente, por defecto se selecciona el día de hoy.
 
-El json del primer argumento tiene el formato siguiente:
+El json del segundo argumento tiene el formato siguiente:
 
 ```json
 {
-   "https://serviciosridi.asse.uy/dcm4chee-arc/qido/DCM4CHEE/instances?Modality=MG&00080080=asseX&00081060=asseX&StudyDate=":"aetSource",
-
+   "asseX":"CHCCL^Nodo",
+   "asseY":"*",
    ...
-
 }
 ```
 
-Los key/value del array contienen el aetSource y el url correspondiente.
+Los key/value del array contienen la institución realizadora de las imágenes y la institución responsable de informarlas o * si se trata de todas las imágenes seleccionadas por el url + institución realizadora de las imágenes.
 
 
 
@@ -59,42 +69,24 @@ Los key/value del array contienen el aetSource y el url correspondiente.
 
 https://support.dcmtk.org/docs/storescu.html
 
-Para cada aetSource
 
 ```
-/usr/local/bin/storescu +sd +sp *.dcm +rn -xv -aet [aetSource] -aec [aetDest] IPDest portDest spoolDirPath/aetSource
+/usr/local/bin/storescu +sd +r +sp *.dcm +rn -R -C -xv -aet [aetSource] -aec [aetDest] IPDest portDest spoolDirPath/-xv
 ```
 
-| argumento   | descripción                                                  |
-| ----------- | ------------------------------------------------------------ |
-| +sd         | scan directory one level                                     |
-| +r          | recurse                                                      |
-| +sp         | scan pattern                                                 |
-| "*.dcm"     | /files ending with .dcm                                      |
-| +rn         | rename with .done or .bad (ignore these files on the next execution) |
-| -R          | only required                                                |
-| -xv         | propose JPEG 2000 lossless TS and all uncompressed transfer syntaxes |
-| -aet        | local aet                                                    |
-| [aetSource] | value associated with qido request                           |
-|             |                                                              |
-|             |                                                              |
-|             |                                                              |
-|             |                                                              |
-|             |                                                              |
-
-
-
-  aet,   //=first segment of path
-
-  @"-aec", //aet of called pacs
-
-  aec,   //=second segment of path
-
-  args[3], //=host of pacs
-
-  args[4], //=port of pacs
-
-  dirPath  //directory to scan
-
-  */
-
+| argumento     | descripción                                                                                                                   |
+| --------------- | ----------------------------------------------------------------------------------------------- |
+| +sd                | scan directory one level                                                                                                |
+| +r                   | recurse                                                                                                                          |
+| +sp                | scan pattern                                                                                                                   |
+| "*.dcm"          | /files ending with .dcm                                                                                                  |
+| +rn                 | rename with .done or .bad (ignore these files on the next execution)                           |
+| -R                   | only required                                                                                                                  |
+| -C                   | combine TS                                                                                                                   |
+| -xv -xs -xe -xi | propose JPEG 2000 lossless jpeg lossless, explicit little endian, implicit little endian |
+| -aet                 | local aet                                                                                                                         |
+| qwprefetch     | value associated with qido request                                                                               |
+| -aec                |  remote aet                                                                                                                    |
+| xxx                  | aet de destino                                                                                                               |
+| ip                    |                                                                                                                                        |
+| puerto             |                                                                                                                                       |
--- main.m
+++ main.m
@@ -8,13 +8,32 @@
 #import "ZZChannel.h"
 #import "ZZError.h"
 
+BOOL addSubdirs(NSFileManager *fileManager, NSString *dir, NSMutableSet *set)
+{
+    NSArray *array=[fileManager contentsOfDirectoryAtPath:dir error:nil];
+    BOOL isDir=false;
+    BOOL hasSubdir=false;
+    for (NSString *content in array)
+    {
+        NSString *subdir=[dir stringByAppendingPathComponent:content];
+        if ([fileManager fileExistsAtPath:subdir isDirectory:&isDir] && isDir)
+            hasSubdir|=addSubdirs(fileManager, subdir, set);
+    }
+    if (!hasSubdir) [set addObject:dir];
+    return true;
+}
+
 int main(int argc, const char * argv[])
 {
  @autoreleasepool {
      
     NSError *error=nil;
 
-     
+    NSData *xv=[@"1.2.840.10008.1.2.4.90" dataUsingEncoding:NSASCIIStringEncoding];//jpeg2000 lossless only
+    NSData *xs=[@"1.2.840.10008.1.2.4.70" dataUsingEncoding:NSASCIIStringEncoding];//jpeg lossless
+    NSData *xe=[@"1.2.840.10008.1.2.1" dataUsingEncoding:NSASCIIStringEncoding];//explicit little endian
+    NSData *xi=[@"1.2.840.10008.1.2" dataUsingEncoding:NSASCIIStringEncoding];//implicit little endian
+    NSRange firstKbRange=NSMakeRange(0, 1024);
 #pragma mark init
      
 
@@ -23,166 +42,206 @@ int main(int argc, const char * argv[])
     //http://unicode.org/reports/tr35/tr35-6.html#Date_Format_Patterns
     NSDateFormatter *DAFormatter = [[NSDateFormatter alloc] init];
     [DAFormatter setDateFormat:@"yyyyMMdd"];
-    NSString *DA=[DAFormatter stringFromDate:[NSDate date]];
 
     //NSDateFormatter *DTFormatter = [[NSDateFormatter alloc] init];
     //[DTFormatter setDateFormat:@"yyyyMMddhhmmss"];
 
-#pragma mark args
+#pragma mark - args
      
      NSMutableArray *args=[NSMutableArray arrayWithArray:[[NSProcessInfo processInfo] arguments]];
      //NSLog(@"%@",[args description]);
      //[0] command path
-     //[1] qidos
-     //[2] spool folder
-     //[3] date (opcional)
+     //[1] qido url
+     //[2] remote reporting orgs json
+     //[3] spool folder
+     //[4] date (opcional)
      
-    /*
+#pragma mark [2] remote reporting orgs json
+     NSData *reportingData=[NSData dataWithContentsOfFile:[args[2] stringByExpandingTildeInPath]];
+     if (!reportingData)
      {
-       "asseX":"https://serviciosridi.asse.uy/dcm4chee-arc/qido/DCM4CHEE/instances?Modality=MG&00080080=asseX&00081060=asseX&StudyDate=",
-        ...
-     }
-     */
-     
-     //[1]qidos
-     NSData *qidosData=[NSData dataWithContentsOfFile:[args[1] stringByExpandingTildeInPath]];
-     if (!qidosData)
-     {
-       NSLog(@"bad path or json qidos file: %@",args[1]);
+       NSLog(@"bad remote reporting orgs path or json file: %@",args[2]);
        exit(1);
      }
-
-     NSDictionary *qidos=[NSJSONSerialization JSONObjectWithData:qidosData options:0 error:&error];
-     if (!qidos)
+     NSDictionary *reporting=[NSJSONSerialization JSONObjectWithData:reportingData options:0 error:&error];
+     if (!reporting)
      {
         NSLog(@"%@",error.description);
         exit(1);
      }
-    
-    //[2] spool path
-     NSString *spoolPath=[args[2] stringByExpandingTildeInPath];
-     for (NSString *qidoKey in [qidos allKeys])
+     //NSLog(@"%@",reporting.description);
+     NSSet *convenios=[NSSet setWithArray:[reporting allKeys]];
+     
+#pragma mark [3] spool path
+     NSString *spoolPath=[args[3] stringByExpandingTildeInPath];
+     
+     NSMutableSet *paths=[NSMutableSet set];
+     addSubdirs(fileManager, spoolPath, paths);
+     
+     NSMutableArray *transferSyntaxes=[NSMutableArray arrayWithArray:[fileManager contentsOfDirectoryAtPath:spoolPath error:nil]];
+     BOOL isDir=false;
+     
+     for (int index=(int)(transferSyntaxes.count)-1; index > -1; index-- )
      {
-        
-        NSString *keySpoolPath=[spoolPath stringByAppendingPathComponent:qidos[qidoKey]];
-        if (
-               ![fileManager fileExistsAtPath:keySpoolPath]
-            && ![fileManager createDirectoryAtPath:keySpoolPath withIntermediateDirectories:YES attributes:nil error:&error]
-           )
-        {
-           NSLog(@"%@",error.description);
-           continue;
-        }
-        
-        NSString *keyDASpoolPath=[keySpoolPath stringByAppendingPathComponent:DA];
-        if (
-               ![fileManager fileExistsAtPath:keyDASpoolPath]
-            && ![fileManager createDirectoryAtPath:keyDASpoolPath withIntermediateDirectories:YES attributes:nil error:&error]
-           )
-        {
-           NSLog(@"%@",error.description);
-           continue;
-        }
-        
-        //NSArray *Es=[fileManager contentsOfDirectoryAtPath:keyDASpoolPath error:&error];
-      
+         if (
+               ![fileManager fileExistsAtPath:[spoolPath stringByAppendingPathComponent:transferSyntaxes[index]] isDirectory:&isDir]
+             ||! isDir
+             ||[transferSyntaxes[index] isEqualToString:@".DS_Store"]
+             )
+             [transferSyntaxes removeObjectAtIndex:index];
+     }
+     
+#pragma mark [4] date
+     NSString *DA=nil;
+     if (args.count==5) DA=args[4];
+     else DA=[DAFormatter stringFromDate:[NSDate date]];
+     
+#pragma mark [1] qido url
+     /*
+      one url only for all orgs!
+      qido institution 00080080 does not work in dcm4chee-arc of Asse
+      */
+     NSString *urlString=args[1];
+     NSURL *qidoURL=[NSURL URLWithString:[urlString stringByAppendingString:DA]];
+     if (!qidoURL) return 1;
+     NSData *qidoResponse=[NSData dataWithContentsOfURL:qidoURL options:NSDataReadingUncached error:&error];
+     if (!qidoResponse)
+     {
+        //NSURLConnection finished with error - code -1002
+        NSLog(@"%@", error.description);
+         return 2;
+     }
+     if (![qidoResponse length]) return 3;//no instance found
+     NSArray *list = [NSJSONSerialization JSONObjectWithData:qidoResponse options:0 error:&error];
+     if (!list)
+     {
+          NSLog(@"%@\r\%@",error.description,[[NSString alloc]initWithData:qidoResponse encoding:NSUTF8StringEncoding] );
+          return 4;
+     }
+     
+#pragma mark - loop InstanceUIDs
 
-#pragma mark qido
-        NSURL *qidoURL=[NSURL URLWithString:[NSString stringWithFormat:@"%@%@",qidoKey,DA]];
-        if (!qidoURL)
-        {
-           NSLog(@"ERROR cannot create url \"%@\":\"%@\"",qidos[qidoKey],[qidoURL absoluteString]);
-           continue;
-        }
-        NSData *qidoResponse=[NSData dataWithContentsOfURL:qidoURL
-                                                   options:NSDataReadingUncached
-                                                     error:&error];
-        if (!qidoResponse)
-        {
-           //NSURLConnection finished with error - code -1002
-           NSLog(@"%@", error.description);
-           continue;
-        }
-        if (![qidoResponse length]) continue;//no instance found
+     for (NSDictionary *item in list)
+     {
+         NSString *Institution=[item[@"00080080"][@"Value"] firstObject];
+         if (![convenios containsObject:Institution]) continue;
+         
+         if (![reporting[Institution] isEqualToString:@"*"])
+         {
+             if (![[item[@"00081060"][@"Value"] firstObject][@"Alphabetic"] hasPrefix:reporting[Institution]]) continue;
+         }
+         
+#pragma mark file already downloaded ?
+         NSString *EUID=[item[@"0020000D"][@"Value"] firstObject];
+         NSString *IUID=[item[@"00080018"][@"Value"] firstObject];
 
-        
-        NSArray *list = [NSJSONSerialization JSONObjectWithData:qidoResponse options:0 error:&error];
-        if (!list)
-        {
-             NSLog(@"qido response to %@ not json. %@ %@",[qidoURL absoluteString],error.description,[[NSString alloc]initWithData:qidoResponse encoding:NSUTF8StringEncoding] );
-             continue;
-        }
-        
-        
-        for (NSDictionary *instance in list)
-        {
-   #pragma mark - loop StudyInstanceUIDs
-
-           NSString *EUID=[instance[@"0020000D"][@"Value"] firstObject];
-           NSString *EUIDpath=[keyDASpoolPath stringByAppendingPathComponent:EUID];
-           if (
-                  ![fileManager fileExistsAtPath:EUIDpath]
-               && ![fileManager createDirectoryAtPath:EUIDpath withIntermediateDirectories:YES attributes:nil error:&error]
-              )
-           {
-              NSLog(@"%@",error.description);
-              continue;
-           }
-           NSString *IUID=[instance[@"00080018"][@"Value"] firstObject];
-
-           NSString *IUIDpath=[EUIDpath stringByAppendingPathComponent:IUID];
-           
-           if (
-                 [fileManager fileExistsAtPath:IUIDpath]
-               &&[fileManager fileExistsAtPath:[IUIDpath stringByAppendingPathExtension:@"OK"]]
-           )
-           {
+         
+         //find study in spool
+         NSString *path=nil;
+         for (NSString *transferSyntax in transferSyntaxes)
+         {
+             NSString *tempPath=[NSString stringWithFormat:@"%@/%@/%@/%@/%@",
+                             spoolPath,
+                             transferSyntax,
+                             Institution,
+                             DA,
+                             EUID];
+             if ([paths containsObject:tempPath])
+             {
+                 path=[NSString stringWithString:tempPath];
+                 break;
+             }
+         }
+         if (path)
+         {
+             NSString *IUIDpath=[[path stringByAppendingPathComponent:IUID] stringByAppendingPathExtension:@"dcm"];
+             if (
+                   [fileManager fileExistsAtPath:IUIDpath]
+                 ||[fileManager fileExistsAtPath:[IUIDpath stringByAppendingPathExtension:@"done"]]
+                 )
+                 continue;
+         }
+         
+         //instance not dowloaded or successfully sent to workstation
 
 #pragma mark download file
-             NSString *RetrieveString=[instance[@"00081190"][@"Value"] firstObject];
-             if (RetrieveString && RetrieveString.length)
-             {
-                NSURL *RetrieveURL=[NSURL URLWithString:RetrieveString];
-                if (!RetrieveURL)
-                {
-                   NSLog(@"ERROR could not create URL from %@",RetrieveString);
-                   continue;
-                }
-                NSData *downloaded=[NSData dataWithContentsOfURL:RetrieveURL
-                                                         options:NSDataReadingUncached
-                                                           error:&error];
-                if (!downloaded)
-                {
-                   NSLog(@"ERROR response to %@: %@",RetrieveString,error.description);
-                   continue;
-                }
+         //NSString *SUID=[instance[@"0020000E"][@"Value"] firstObject];
+
+         NSString *RetrieveString=[item[@"00081190"][@"Value"] firstObject];
+         if (RetrieveString && RetrieveString.length)
+         {
+            NSURL *RetrieveURL=[NSURL URLWithString:RetrieveString];
+            if (!RetrieveURL)
+            {
+                NSLog(@"ERROR could not create URL from %@",RetrieveString);
+                continue;
+            }
+            NSData *downloaded=[NSData dataWithContentsOfURL:RetrieveURL options:NSDataReadingUncached error:&error];
+            if (!downloaded)
+            {
+               NSLog(@"ERROR response to %@: %@",RetrieveString,error.description);
+               continue;
+            }
 
                                      
-                if (downloaded.length == 0)
-                {
-                    NSLog(@"empty response to %@",RetrieveString);
-                    continue;
-                }
+            if (downloaded.length < 1000)
+            {
+               NSLog(@"empty response to %@",RetrieveString);
+               continue;
+            }
                  
-                 //unzip
-                 ZZArchive *archive = [ZZArchive archiveWithData:downloaded];
-                 ZZArchiveEntry *firstEntry = archive.entries[0];
-                 NSData *unzipped = [firstEntry newDataWithError:&error];
-                 if (error!=nil)
-                 {
-                     NSLog(@"could NOT unzip response to %@: %@",RetrieveString,downloaded.description);
+#pragma mark unzip
+            ZZArchive *archive = [ZZArchive archiveWithData:downloaded];
+            ZZArchiveEntry *firstEntry = archive.entries[0];
+            NSData *unzipped = [firstEntry newDataWithError:&error];
+            if (error!=nil)
+            {
+               NSLog(@"could NOT unzip response to %@: %@",RetrieveString,downloaded.description);
                      continue;
-                 }
-                 
-                 [unzipped writeToFile:IUIDpath atomically:NO];
-             }//end retrieveString
-              
-           }
+            }
+            
+#pragma mark find transfer syntax
+            NSString *transferSyntax=nil;
+            NSRange xvRange=[unzipped rangeOfData:xv options:0 range:firstKbRange];
+            if (xvRange.location==NSNotFound)
+            {
+                NSRange xsRange=[unzipped rangeOfData:xs options:0 range:firstKbRange];
+                if (xsRange.location==NSNotFound)
+                {
+                    NSRange xeRange=[unzipped rangeOfData:xe options:0 range:firstKbRange];
+                    if (xeRange.location==NSNotFound)
+                    {
+                        NSRange xiRange=[unzipped rangeOfData:xi options:0 range:firstKbRange];
+                        if (xiRange.location==NSNotFound)
+                        {
+                            NSLog(@"unknown transfer syntax for  %@",RetrieveString);
+                            transferSyntax=@"unknown";
+                        }
+                        else transferSyntax=@"-xi";
+                    }
+                    else transferSyntax=@"-xe";
+                }
+                else transferSyntax=@"-xs";
+            }
+            else transferSyntax=@"-xv";
+             
+#pragma mark mkdir?
+            if (!path)
+            {
+                path=[NSString stringWithFormat:@"%@/%@/%@/%@/%@",
+                                spoolPath,
+                                transferSyntax,
+                                Institution,
+                                DA,
+                                EUID];
+               [fileManager createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
+               [paths addObject:path];
+            }
 
+#pragma mark write file
+             [unzipped writeToFile:[[path stringByAppendingPathComponent:IUID] stringByAppendingPathExtension:@"dcm"] atomically:NO];
         }
-        
-
      }
   }
-   return 0;
+  return 0;
 }
